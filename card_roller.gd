f18c58d8ea0aab999707fd4834d74a2272119d70
Blue-Kobold <72634056+Blue-Kobold@users.noreply.github.com>
2023-11-06 03:38:16 -0500

    Expanding card_roller.gd

--- card_roller.gd
+++ card_roller.gd
@@ -5,18 +5,146 @@ extends Node2D
 var card : Dictionary = {}
 var rng
 
-func _ready():
-	rng = RandomNumberGenerator.new()
-	for i in number_of_cards_to_gen:
-		print(roll_for_card_type(rnginator(2)))
-		
-func rnginator(i):
-	rng.randomize()
-	return rng.randi_range(1, i)
+var affinities : Array = [
+	"Order",
+	"Mind",
+	"Ambition",
+	"Chaos",
+	"Nature",
+	"Spirit",
+	"None"
+]
+
+var soul_types : Dictionary = {
 	
-func roll_for_card_type(i):
-	match i:
+	"Order" : {
+		"None" : "Paladin",
+		"Mind" : "Construct",
+		"Ambition" : "Noble",
+		"Nature" : "Hunter",
+		"Spirit" : "Angel"},
+		
+	"Mind" : {
+		"None" : "Wizard",
+		"Ambition" : "Rogue",
+		"Chaos" : "Horror",
+		"Spirit" : "Cleric",
+		"Order" : "Construct"},
+		
+	"Ambition" : {
+		"None" : "Dragon",
+		"Chaos" : "Demon",
+		"Nature" : "Dinosaur",
+		"Order" : "Noble",
+		"Mind" : "Rogue"},
+		
+	"Chaos" : {
+		"None" : "Warrior",
+		"Nature" : "Fae",
+		"Spirit" : "Cultist",
+		"Mind" : "Horror",
+		"Ambition" : "Demon"},
+		
+	"Nature" : {
+		"None" : "Beast",
+		"Spirit" : "Elemental",
+		"Order" : "Hunter",
+		"Ambition" : "Dinosaur",
+		"Chaos" : "Fae"},
+		
+	"Spirit" : {
+		"None" : "Undead",
+		"Order" : "Angel",
+		"Mind" : "Cleric",
+		"Chaos" : "Cultist",
+		"Nature" : "Elemental"}
+	}
+
+
+func _ready():
+	generate_card()
+
+
+func generate_card():
+	for i in number_of_cards_to_gen:
+		card.merge(roll_for_card_affinity(rnginator(8)))
+		match roll_for_card_type(rnginator(2)):
+			"Soul":
+				card.merge({"Card Type" : "Soul"})
+				card.merge(roll_for_soul_type(rnginator(12)))
+				print(str(card))
+				clear_card()
+			"Magic":
+				card.merge({"Card Type" : "Magic"})
+				print(str(card))
+				clear_card()
+	
+	
+func clear_card():
+	card.clear()
+	
+
+func rnginator(dice_size):
+	rng = RandomNumberGenerator.new()
+	rng.randomize()
+	return rng.randi_range(1, dice_size)
+	
+	
+func roll_for_card_type(card_type_roll_result):
+	match card_type_roll_result:
 		1:
 			return "Soul"
 		2:
 			return "Magic"
+
+
+func roll_for_card_affinity(card_affinity_roll_result):
+	match card_affinity_roll_result:
+		1:
+			return { "Primary Affinity" : "Order",
+					 "Secondary Affinity" : "None" }
+		2:
+			return { "Primary Affinity" : "Mind",
+					 "Secondary Affinity" : "None" }
+		3:
+			return { "Primary Affinity" : "Ambition",
+					 "Secondary Affinity" : "None" }
+		4:
+			return { "Primary Affinity" : "Chaos",
+					 "Secondary Affinity" : "None" }
+		5:
+			return { "Primary Affinity" : "Nature",
+					 "Secondary Affinity" : "None" }
+		6:
+			return { "Primary Affinity" : "Spirit",
+					 "Secondary Affinity" : "None" }
+		7, 8:
+			var primary_affinity = roll_for_card_affinity(rnginator(6))
+			var secondary_affinity = roll_for_card_affinity(rnginator(6))
+			while primary_affinity["Primary Affinity"] == secondary_affinity["Primary Affinity"]:
+				secondary_affinity = roll_for_card_affinity(rnginator(6))
+			return { "Primary Affinity" : str(primary_affinity["Primary Affinity"]),
+					 "Secondary Affinity" : str(secondary_affinity["Primary Affinity"])}
+					
+
+func roll_for_soul_type(soul_type_roll_result):
+	match soul_type_roll_result:
+		1,2,3,4:
+			return { 
+				"Primary Soul Type" : soul_types[card["Primary Affinity"]][card["Secondary Affinity"]],
+				"Secondary Soul Type" : "None"}
+		5,6,7,8,9,10:
+			var off_soul_type = soul_types[card["Primary Affinity"]].values().pick_random()
+			while off_soul_type == soul_types[card["Primary Affinity"]][card["Secondary Affinity"]]:
+				off_soul_type = soul_types[card["Primary Affinity"]].values().pick_random()
+			return {
+				"Primary Soul Type" : off_soul_type,
+				"Secondary Soul Type" : "None"}
+		11,12:
+			var secondary_soul_type = soul_types[card["Primary Affinity"]].values().pick_random()
+			while secondary_soul_type == soul_types[card["Primary Affinity"]][card["Secondary Affinity"]]:
+				secondary_soul_type = soul_types[card["Primary Affinity"]].values().pick_random()
+			return {
+				"Primary Soul Type" : soul_types[card["Primary Affinity"]][card["Secondary Affinity"]],
+				"Secondary Soul Type" : secondary_soul_type}
+		
